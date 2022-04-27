f445ecd782492e9d4469c14f6fefdf514a6fb050
ZoletaDezza <zoletadezza101@gmail.com>
2022-04-27 22:47:40 +0800

    images added

--- account.php
+++ account.php
@@ -30,7 +30,7 @@ if (isset($_SESSION['ses_username'])===false) {
     transform: translateY(5rem)
 }
 .cover {
-    background-image: url('images/photo-profile-background.avif');
+    background-image: url('images/background.avif');
     background-size: cover;
     background-repeat: no-repeat
 }
@@ -49,7 +49,7 @@ body {
         <div class="bg-white shadow rounded overflow-hidden">
             <div class="px-4 pt-0 pb-4 cover">
                 <div class="media align-items-end profile-head">
-                    <div class="profile mr-3"><img src="images/photo-profile.avif" alt="..." width="150" class="rounded mb-2 img-thumbnail">
+                    <div class="profile mr-3"><img src="images/profile-photo.avif" alt="..." width="150" class="rounded mb-2 img-thumbnail">
                         <a href="?logout" class="btn btn-outline-dark btn-sm btn-block">Sign Out</a>
 
 
@@ -90,10 +90,10 @@ body {
                 </div>
                 <div class="row">
                     
-                    <div class="col-lg-6 mb-2 pr-lg-1"><img src="images/photo-1.avif" alt="" class="img-fluid rounded shadow-sm"></div>              
-                    <div class="col-lg-6 mb-2 pl-lg-1"><img src="images/photo-2.avif" alt="" class="img-fluid rounded shadow-sm"></div>                
-                    <div class="col-lg-6 pr-lg-1 mb-2"><img src="images/photo-3.avif" alt="" class="img-fluid rounded shadow-sm"></div>
-                    <div class="col-lg-6 pl-lg-1"><img src="images/photo-4.avif" alt="" class="img-fluid rounded shadow-sm"></div>
+                    <div class="col-lg-6 mb-2 pr-lg-1"><img src="images/image-1.avif" alt="" class="img-fluid rounded shadow-sm"></div>              
+                    <div class="col-lg-6 mb-2 pl-lg-1"><img src="images/image-2.avif" alt="" class="img-fluid rounded shadow-sm"></div>                
+                    <div class="col-lg-6 pr-lg-1 mb-2"><img src="images/image-3.avif" alt="" class="img-fluid rounded shadow-sm"></div>
+                    <div class="col-lg-6 pl-lg-1"><img src="images/image-4.avif" alt="" class="img-fluid rounded shadow-sm"></div>
                 </div> 
             </div>
 
